5d6cada029e76e9f7c8a5e4efbb8f68cdbb7d225
Asuki Kono <asukiaaa@gmail.com>
2022-12-29 23:40:45 +0900

    Remove not used macro

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,8 @@
 // https://github.com/adafruit/Adafruit_nRF52_Arduino/blob/master/libraries/Bluefruit52Lib/examples/Central/central_custom_hrm/central_custom_hrm.ino
 // https://github.com/adafruit/Adafruit_nRF52_Arduino/blob/master/libraries/Bluefruit52Lib/examples/Central/central_hid/central_hid.ino
 
-#define EXAMPLE_SCAN_ADVANCE
-#ifdef EXAMPLE_SCAN_ADVANCE
-/*********************************************************************
- This is an example for our nRF52 based Bluefruit LE modules
-
- Pick one up today in the adafruit shop!
-
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
 
 #include <bluefruit.h>
-
-/* For a list of EIR data types see:
- *    https://www.bluetooth.com/specifications/assigned-numbers/generic-access-profile
- *    Matching enum: cores/nRF5/SDK/components/softdevice/s132/headers/ble_gap.h
- */
-
-// BLEClientService        hrms(UUID16_SVC_HEART_RATE);
-// BLEClientCharacteristic hrmc(UUID16_CHR_HEART_RATE_MEASUREMENT);
-// BLEClientCharacteristic bslc(UUID16_CHR_BODY_SENSOR_LOCATION);
 
 BLEClientService serviceBattery(UUID16_SVC_BATTERY);
 BLEClientCharacteristic charaBattery(UUID16_CHR_BATTERY_LEVEL);
@@ -338,18 +314,6 @@ void connection_secured_callback(uint16_t conn_handle) {
       // handle_service_battery(conn_handle);
       handle_service_battery(conn_handle);
     }
-
-    // // BLEClientHidAdafruit currently only supports Boot Protocol Mode
-    // // for Keyboard and Mouse. Let's set the protocol mode on prph to Boot
-    // Mode hid.setBootMode(true);
-
-    // // Enable Keyboard report notification if present on prph
-    // if (hid.keyboardPresent()) hid.enableKeyboard();
-
-    // // Enable Mouse report notification if present on prph
-    // if (hid.mousePresent()) hid.enableMouse();
-
-    // Serial.println("Ready to receive from peripheral");
   }
 }
 
@@ -409,62 +373,5 @@ void setup() {
 
 void loop() {
   // nothing to do
+  delay(10);
 }
-#endif
-
-#ifdef EXAMPLE_SCAN_BASIC
-#include <bluefruit.h>
-
-void scan_callback(ble_gap_evt_adv_report_t* report) {
-  Serial.println("Timestamp Addr              Rssi Data");
-
-  Serial.printf("%09d ", millis());
-
-  // MAC is in little endian --> print reverse
-  Serial.printBufferReverse(report->peer_addr.addr, 6, ':');
-  Serial.print(" ");
-
-  Serial.print(report->rssi);
-  Serial.print("  ");
-
-  Serial.printBuffer(report->data.p_data, report->data.len, '-');
-  Serial.println();
-
-  // Check if advertising contain BleUart service
-  if (Bluefruit.Scanner.checkReportForUuid(report, BLEUART_UUID_SERVICE)) {
-    Serial.println("                       BLE UART service detected");
-  }
-
-  Serial.println();
-
-  // For Softdevice v6: after received a report, scanner will be paused
-  // We need to call Scanner resume() to continue scanning
-  Bluefruit.Scanner.resume();
-}
-
-void setup() {
-  Serial.begin(115200);
-  while (!Serial) delay(10);  // for nrf52840 with native usb
-
-  Serial.println("Bluefruit52 Central Scan Example");
-  Serial.println("--------------------------------\n");
-
-  // Initialize Bluefruit with maximum connections as Peripheral = 0, Central =
-  // 1 SRAM usage required by SoftDevice will increase dramatically with number
-  // of connections
-  Bluefruit.begin(0, 1);
-  Bluefruit.setTxPower(4);  // Check bluefruit.h for supported values
-  Bluefruit.setName("Bluefruit52");
-
-  // Start Central Scan
-  Bluefruit.setConnLedInterval(250);
-  Bluefruit.Scanner.setRxCallback(scan_callback);
-  Bluefruit.Scanner.start(0);
-
-  Serial.println("Scanning ...");
-}
-
-void loop() {
-  // nothing to do
-}
-#endif
